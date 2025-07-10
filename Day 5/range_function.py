8d545ef02e1398d4eb21a9829ee037d50edc2eec
Kashmin Shrestha <kashmin44@gmail.com>
2025-07-10 21:16:30 +0545

    day 5

---
+++ Day 5/range_function.py
@@ -0,0 +1,27 @@
+# range(5)  # 0,1,2,3,4
+# # range(stop)
+
+# range(2, 5)  # 2,3,4
+# # range(start, stop)
+
+
+# range(2, 10, 2)  # 2,4,6,8
+# # range(start, stop, step)
+
+# range(10, 2, -2)  # 10,8,6,4,2
+# # range(start, stop, step)
+# # # start is inclusive, stop is exclusive
+
+# for i in range(5): 
+#     #[]
+#     print(i)  # Output: 0 1 2 3 4
+
+# for durga in range (1,7):
+#     #[1,2,3,4,5,6]
+#     print(durga , end=",")  # Output: 0 1 2 3 4 5 6
+    
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)  # Output: apple banana cherry
+
